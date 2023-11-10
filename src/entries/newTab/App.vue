00ac0fb57ada28124fa6db42d12f8b3312cd547b
Importantus <importantus@mailbox.org>
2023-11-10 22:39:56 +0100

    ✨ Add new components to page

--- src/entries/newTab/App.vue
+++ src/entries/newTab/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import BottomBar from "./components/BottomBar.vue";
 import Image from "./components/Image.vue";
+import LoadingScreen from "./components/LoadingScreen.vue";
+import ReloadButton from "./components/ReloadButton.vue";
+import WikiInfo from "./components/WikiInfo.vue";
+import ImagefitSelector from "./components/ImagefitSelector.vue";
 
 import { useDataStore } from "./store";
 
@@ -10,16 +15,67 @@ dataStore.init();
 
 <template>
   <main>
+    <LoadingScreen />
     <Image />
+    <BottomBar>
+      <div class="right">
+        <ReloadButton />
+        <div class="infos">
+          <WikiInfo :wikipediaData="dataStore.imageTitle" big />
+          <div class="subtitle">
+            <WikiInfo :wikipediaData="dataStore.artist" :big="false" />
+            <div>•</div>
+            {{ dataStore.imageData.metadata.date === "NaN" ? "unknown" : dataStore.imageData.metadata.date }}
+            <div>•</div>
+            <WikiInfo :wikipediaData="dataStore.museum" :big="false" />
+          </div>
+        </div>
+      </div>
+      <ImagefitSelector />
+    </BottomBar>
   </main>
 </template>
 
 <style>
+.infos {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem;
+}
+
+.right {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex: 1;
+  gap: 0.9rem;
+}
+
+.subtitle {
+  word-break: normal;
+  flex-wrap: wrap;
+  text-wrap: wrap;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 0.5rem;
+  color: rgb(171, 171, 171);
+  font-size: 0.9rem;
+  font-weight: 300;
+}
+
+.a {
+  color: rgb(171, 171, 171);
+  font-size: 0.9rem;
+  font-weight: 300;
+}
+
 main {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 </style>
