9e864d55555c945395e85ca83bc05a573b7924b9
Importantus <importantus@mailbox.org>
2023-11-10 22:37:39 +0100

    ✨ Show infos

---
+++ src/entries/newTab/components/WikiInfo.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { WikipediaData } from '../store'
+
+interface Props {
+    wikipediaData: WikipediaData
+    big: boolean
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <a :href="props.wikipediaData.wikipedia?.url" :class="{ 'big': props.big }">
+        {{ props.wikipediaData.label?.value || "Unknown" }}
+    </a>
+</template>
+
+<style scoped>
+a {
+    color: inherit;
+    text-decoration: none;
+    text-align: left;
+    transition: ease;
+    transition-duration: 150ms;
+}
+
+a[href*="://"]:hover {
+    color: white;
+    text-decoration: underline;
+}
+
+.big {
+    color: white;
+    font-size: 1.5rem;
+    font-weight: 400;
+}
+</style>
