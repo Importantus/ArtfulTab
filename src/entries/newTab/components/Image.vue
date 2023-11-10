f05048a894073702267b620f133b67d71dd65b89
Importantus <importantus@mailbox.org>
2023-11-10 22:38:46 +0100

    ✨ Show image

--- src/entries/newTab/components/Image.vue
+++ src/entries/newTab/components/Image.vue
@@ -1,12 +1,48 @@
 <script lang="ts" setup>
-import { useDataStore } from '../store';
+import { imageFit, useDataStore } from '../store';
 
 const dataStore = useDataStore();
 </script>
 
 <template>
-    <p v-if="dataStore.loading">
-        Loading...
-    </p>
-    <img v-if="dataStore.imageData.files !== undefined" :src="dataStore.getThumbnailUrl" />
+    <div v-if="dataStore.imageData.files !== undefined" class="image-container">
+        <div class="fill" :style="{ backgroundImage: 'url(' + dataStore.thumbnailUrl + ')' }"
+            :class="{ 'blur': dataStore.settings.imageFit.id !== imageFit.fill.id }"></div>
+        <img v-if="dataStore.imageData.files !== undefined && dataStore.settings.imageFit.id === imageFit.fit.id"
+            :src="dataStore.thumbnailUrl" />
+    </div>
 </template>
+
+<style scoped>
+.image-container {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.fill {
+    position: absolute;
+    width: 110%;
+    margin-top: -20px;
+    margin-left: -20px;
+    background-size: cover;
+    height: 110%;
+    background-position: center;
+    z-index: 0;
+}
+
+.blur {
+    filter: blur(100px);
+}
+
+img {
+    z-index: 1;
+    border-radius: 2px;
+    max-height: 90%;
+    max-width: 100%;
+}
+</style>
