b6275be2b977a7e9a60656ca0129d41d73ba2eee
Importantus <importantus@mailbox.org>
2023-11-10 22:39:33 +0100

    ✨ Allow changing of imagefit

---
+++ src/entries/newTab/components/ImagefitItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { useDataStore, ImageFit } from '../store';
+
+interface Props {
+    imageFitValue: ImageFit;
+}
+
+const props = defineProps<Props>();
+
+const dataStore = useDataStore();
+</script>
+
+<template>
+    <div :class="{ 'selected': props.imageFitValue.id === dataStore.settings.imageFit.id }"
+        @click="dataStore.setImageFit(props.imageFitValue)">
+        <props.imageFitValue.icon :size="18"
+            :color="props.imageFitValue.id === dataStore.settings.imageFit.id ? 'rgba(255, 255, 255, 0.6)' : 'rgba(255, 255, 255, 0.3)'" />
+    </div>
+</template>
+
+<style scoped>
+div {
+    width: 50px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+/* Divs on hover that dont have the class selected */
+div:hover:not(.selected) {
+    background-color: rgba(255, 255, 255, 0.05)
+}
+
+.selected {
+    background-color: rgba(255, 255, 255, 0.1)
+}
+</style>
---
+++ src/entries/newTab/components/ImagefitSelector.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { imageFit } from '../store';
+import ImagefitItem from './ImagefitItem.vue';
+</script>
+
+<template>
+    <div class="selector">
+        <ImagefitItem v-for="value in imageFit" :key="value.id" :imageFitValue="value" />
+    </div>
+</template>
+
+<style scoped>
+.selector {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border-radius: 50px;
+    overflow: hidden;
+    background-color: rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+    /* blur background */
+    backdrop-filter: blur(5px);
+}
+
+/* First and last items have padding left/right */
+.selector>*:first-child {
+    padding-left: 5px;
+}
+
+.selector>*:last-child {
+    padding-right: 5px;
+}
+</style>
